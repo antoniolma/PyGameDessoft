35232fb1338e774249e9a4aec2d2143d7dcd5091
Verônica Lima <veronicalrl@al.insper.edu.br>
2022-05-31 09:58:11 -0300

    trocando a bala

--- settings.py
+++ settings.py
@@ -16,5 +16,5 @@ tile_size = 64                                # Tamanho do tile (Bloco de chão)
 screen_width = 768                            # Largura do tile  (20 tiles)
 screen_height = len(level_map) * tile_size                           # Altura do tile (11 tiles)
 
-print(screen_height)
-print(len(level_map[0])*tile_size)
+#print(screen_height)
+#print(len(level_map[0])*tile_size)
